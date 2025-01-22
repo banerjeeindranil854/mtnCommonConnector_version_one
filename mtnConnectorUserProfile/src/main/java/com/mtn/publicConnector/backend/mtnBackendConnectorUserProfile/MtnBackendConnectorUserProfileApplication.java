60d32da76566d962e1a4770b6c5a1a82110b674e
Indranil Banerjee <banerjeeindranil854@gmail.com>
2025-01-22 22:28:33 +0000

    added for changes

---
+++ mtnConnectorUserProfile/src/main/java/com/mtn/publicConnector/backend/mtnBackendConnectorUserProfile/MtnBackendConnectorUserProfileApplication.java
@@ -0,0 +1,25 @@
+
+package com.mtn.publicConnector.backend.mtnBackendConnectorUserProfile;
+
+import com.mtn.publicConnector.config.template.YamlPropertySourceFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ConfigurationProperties(prefix = "yaml")
+@PropertySources(
+		{@PropertySource(value = "classpath:application.yaml",ignoreResourceNotFound = false,factory = YamlPropertySourceFactory.class),
+				@PropertySource(value = "classpath:application.properties" ,ignoreResourceNotFound = false)
+		}
+)
+public class MtnBackendConnectorUserProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtnBackendConnectorUserProfileApplication.class, args);
+	}
+
+}
